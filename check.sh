d232eb76e7f2dc5fe5e616f9776b56ed4ac0670c
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2024-02-23 01:14:10 +0100

    feat: integrate https://github.com/TianyiShi2001/hhmmss to temps
    - use features to separate "chrono" / "time" dependencies

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cargo fmt
-cargo clippy --tests
-cargo nextest run
+cargo fmt --all
+cargo clippy --tests --all-features --all-targets
+cargo test --tests --all-features --all-targets
